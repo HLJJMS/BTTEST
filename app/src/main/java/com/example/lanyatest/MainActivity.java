33b1cc437f5ad1d4c983eb6498c0d255a20ed667
administered <958245365@qq.com>
2020-04-22 14:59:40 +0800

    界面补全

--- app/src/main/java/com/example/lanyatest/MainActivity.java
+++ app/src/main/java/com/example/lanyatest/MainActivity.java
@@ -7,6 +7,7 @@ import android.bluetooth.BluetoothGatt;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Gravity;
@@ -31,6 +32,15 @@ import com.clj.fastble.callback.BleScanCallback;
 import com.clj.fastble.data.BleDevice;
 import com.clj.fastble.exception.BleException;
 import com.clj.fastble.scan.BleScanRuleConfig;
+import com.inuker.bluetooth.library.BluetoothClient;
+import com.inuker.bluetooth.library.beacon.Beacon;
+import com.inuker.bluetooth.library.connect.options.BleConnectOptions;
+import com.inuker.bluetooth.library.connect.response.BleConnectResponse;
+import com.inuker.bluetooth.library.model.BleGattProfile;
+import com.inuker.bluetooth.library.search.SearchRequest;
+import com.inuker.bluetooth.library.search.SearchResult;
+import com.inuker.bluetooth.library.search.response.SearchResponse;
+import com.inuker.bluetooth.library.utils.BluetoothLog;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.util.ArrayList;
@@ -40,6 +50,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.functions.Consumer;
+
+import static com.inuker.bluetooth.library.Code.REQUEST_SUCCESS;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
@@ -152,6 +164,7 @@ public class MainActivity extends AppCompatActivity {
             public void accept(Boolean aBoolean) throws Exception {
                 if (aBoolean) {
                     Log.e("结果", aBoolean.toString());
+//                    aboutBlueTooth();
                 } else {
                     Log.e("结果", aBoolean.toString());
                 }
@@ -332,6 +345,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.img_bt:
                 aboutBlueTooth();
+
+
                 break;
         }
     }
@@ -386,7 +401,7 @@ public class MainActivity extends AppCompatActivity {
         BleScanRuleConfig scanRuleConfig = new BleScanRuleConfig.Builder()
 //                .setServiceUuids(serviceUuids)      // 只扫描指定的服务的设备，可选
 //                .setDeviceName(true, "Honor Play")         // 只扫描指定广播名的设备，可选
-//                .setDeviceMac("7c:76:68:db:8c:b0")                  // 只扫描指定mac的设备，可选
+//                .setDeviceMac("4C:7C:5F:B0:94:80")                  // 只扫描指定mac的设备，可选
 //                .setAutoConnect(isAutoConnect)      // 连接时的autoConnect参数，可选，默认false
                 .setScanTimeOut(10000)              // 扫描超时时间，可选，默认10秒；小于等于0表示不限制扫描时间
                 .build();
@@ -448,14 +463,104 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onScanFinished(List<BleDevice> scanResultList) {
                 // 扫描结束，列出所有扫描到的符合扫描规则的BLE设备（主线程）
-                if(scanResultList.size()==0){
+                for (int i = 0; i < scanResultList.size(); i++) {
+                    String a = scanResultList.get(i).getMac();
+                    String b = scanResultList.get(i).getName();
+                    String c = scanResultList.get(i).getDevice().getName();
+                    String d = scanResultList.get(i).getDevice().getAddress();
+                    ParcelUuid[] f = scanResultList.get(i).getDevice().getUuids();
 
-                }else{
+                    BleManager.getInstance().connect(scanResultList.get(i), new BleScanAndConnectCallback() {
+                        @Override
+                        public void onScanFinished(BleDevice scanResult) {
 
+                        }
+
+                        @Override
+                        public void onStartConnect() {
+
+                        }
+
+                        @Override
+                        public void onConnectFail(BleDevice bleDevice, BleException exception) {
+                            Log.e("连接失败", exception.toString());
+                        }
+
+                        @Override
+                        public void onConnectSuccess(BleDevice bleDevice, BluetoothGatt gatt, int status) {
+                            // 连接成功，BleDevice即为所连接的BLE设备（主线程）
+//                Log.e("成功链接", bleDevice.toString());
+                        }
+
+                        @Override
+                        public void onDisConnected(boolean isActiveDisConnected, BleDevice device, BluetoothGatt gatt, int status) {
+
+                        }
+
+                        @Override
+                        public void onScanStarted(boolean success) {
+
+                        }
+
+                        @Override
+                        public void onScanning(BleDevice bleDevice) {
+
+                        }
+                    });
                 }
             }
         });
 
+
+    }
+
+
+    private void kitBt() {
+        BluetoothClient mClient = new BluetoothClient(context);
+        SearchRequest request = new SearchRequest.Builder()
+                .searchBluetoothLeDevice(3000, 3)   // 先扫BLE设备3次，每次3s
+                .searchBluetoothClassicDevice(5000) // 再扫经典蓝牙5s
+                .searchBluetoothLeDevice(2000)      // 再扫BLE设备2s
+                .build();
+
+        BleConnectOptions options = new BleConnectOptions.Builder()
+                .setConnectRetry(3)   // 连接如果失败重试3次
+                .setConnectTimeout(30000)   // 连接超时30s
+                .setServiceDiscoverRetry(3)  // 发现服务如果失败重试3次
+                .setServiceDiscoverTimeout(20000)  // 发现服务超时20s
+                .build();
+        mClient.search(request, new SearchResponse() {
+            @Override
+            public void onSearchStarted() {
+
+            }
+
+            @Override
+            public void onDeviceFounded(SearchResult device) {
+                Beacon beacon = new Beacon(device.scanRecord);
+//                BluetoothLog.e(String.format("beacon for %s\n%s", device.getAddress(), beacon.toString()));
+                Log.e(device.getAddress(), "哈哈哈哈哈哈哈");
+            }
+
+            @Override
+            public void onSearchStopped() {
+
+            }
+
+            @Override
+            public void onSearchCanceled() {
+
+            }
+        });
+
+        mClient.connect("7C:76:68:DB:39:D6", options, new BleConnectResponse() {
+            @Override
+            public void onResponse(int code, BleGattProfile data) {
+                if (code == REQUEST_SUCCESS) {
+
+                }
+            }
+        });
 
     }
 }
